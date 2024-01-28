f619d8bbfc20c708a720d3d886d23a7ea2981f34
yassinMi <dir16cat17@gmail.com>
2024-01-28 20:03:12 +0000

    fixed casing issue

--- src/RecordingRoomCard.tsx
+++ src/RecordingRoomCard.tsx
@@ -37,7 +37,7 @@ export const RecordingRoomCard: React.FC<RecordingRoomCard_Props> = ({ maximizeC
             </div>
             <button className="icon-action-button icon16" onClick={hndlMaximizeClick}>
             <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+  <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
 </svg>
             </button>
             
